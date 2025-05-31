89167ebc7c7b568ad15eee32af6e1d3be2395033
ByronPrado <byron.prado@sansano.usm.cl>
2025-05-31 11:02:58 -0400

    Orden Codigo e Implementacion logica de botones.
    Realizamos cambios importantes en MainViewModel ahora si se actualizan los datos en tiempo real, agregamos el comportamiento de los botones y mejoramos la logica de negocio al hacer uso de la función OnValorBoletaChanged en vez de de funcion MontoBoleta.
    Falta revisar el orden del codigo, así tambien el aspecto visual del mainPage.xmal
    
    Co-Authored-By: Fcoegg <119830289+Fcoegg@users.noreply.github.com>

--- Proyecto2/ViewModel/mainViewModel.cs
+++ Proyecto2/ViewModel/mainViewModel.cs
@@ -27,13 +27,14 @@ public partial class MainViewModel : ObservableObject
 
     [RelayCommand]
     public void CalcularMontoTotal()
-    {
+    {   
+        // Esta funcion calcula el monto total de la boleta y la propina
         if (DivisorMonto > 0)
         {
             double propina = ValorBoleta * PropinaPorcentaje / 100;
             double boleta = (ValorBoleta + propina) / DivisorMonto;
             MontoTotal = (int)Math.Round(boleta, 0);
-            PropinaMonto = (int)Math.Round(propina/DivisorMonto, 0);
+            PropinaMonto = (int)Math.Round(propina / DivisorMonto, 0);
         }
         else
         {
@@ -41,42 +42,59 @@ public partial class MainViewModel : ObservableObject
             PropinaMonto = 0;
         }
     }
+
     [RelayCommand]
-    public void MontoBoleta(int nuevoValor)
+    public void CantidadPersonas(string op)
     {
-        if (nuevoValor >= 0)
+        if (op == "-")
         {
-            ValorBoleta = nuevoValor;
+            if (DivisorMonto > 1)
+            {
+                DivisorMonto--;
+                CalcularMontoTotal();
+            }
+        }
+        else if (op == "+")
+        {
+            DivisorMonto++;
+            CalcularMontoTotal();
+        }
+    }
+
+    [RelayCommand]
+    public void EditarPropina(string nuevoValor)
+    {
+        //esta funcion la utilizamos para los botones con propinas predefinidas
+        if (int.TryParse(nuevoValor, out int porcentaje))
+        {
+            PropinaPorcentaje = porcentaje;
+        }
+        else
+        {
+            PropinaPorcentaje = 0; // Si no se puede convertir lo dejamos en 0
+        }
+        CalcularMontoTotal();
+    }
+
+    partial void OnPropinaPorcentajeChanged(int value)
+    {
+        // Esta funcion la utilizamos para los sliders ya que cambia directo el valor 
+        CalcularMontoTotal();
+    }
+    partial void OnValorBoletaChanged(int value)
+    {
+        Console.WriteLine($"ValorBoleta tipo: {value.GetType()}");
+        if (value >= 0)
+        {
+            ValorBoleta = value;
             CalcularMontoTotal();
         }
         else
         {
-            nuevoValor = 0;
-            ValorBoleta = nuevoValor;
-        }
-    }
-
-    [RelayCommand]
-    public void DisminuirDivisor()
-    {
-        if (DivisorMonto > 1)
-        {
-            DivisorMonto--;
+            value = 0;
+            ValorBoleta = value;
             CalcularMontoTotal();
         }
-    }
-    [RelayCommand]
-    public void AumentarDivisor()
-    {
-        DivisorMonto++;
-        CalcularMontoTotal();        
-    }
-
-    [RelayCommand]
-    public void EditarPropina(int nuevoValor)
-    {
-        PropinaPorcentaje = nuevoValor;
-        CalcularMontoTotal();
     }
 
 
